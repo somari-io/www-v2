b3b55e02e59a08d3ae9563c8c371e8c0fe215b3b
Guillaume Deflaux <guillaume@deflaux.net>
2024-04-04 23:36:32 +0200

    add somari colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./layouts/**/*.html', './content/**/*.md'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'somari': {
+          light: '#f68722',
+          DEFAULT: '#f25a2d',
+          dark: '#ef2b43',
+        }
+    },
+    
+    }
   },
   plugins: [
     require("@tailwindcss/typography"),
